7ffe2d3db8ea50328c89ede4d0fc7bc342f22076
Rishesha Harsh <risheshaharsh@gmail.com>
2025-10-11 18:28:49 +0530

    Implement loading and error boundary components; enhance blog and review details pages with async functionality

--- hello-world/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ hello-world/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,14 +1,25 @@
 import { notFound , redirect} from "next/navigation"
 
+
+function getRandomInt(count : number){
+    return Math.floor(Math.random()*count)
+}
+
 export default async function ReviewDetails({
     params,
 }: {
     params : Promise<{productId: string, reviewsId : string}>
-}){
+}) {
+    const random = getRandomInt(2)
+    if(random ===0){
+        throw new Error("Error loading view")
+    }
+{
 const {productId, reviewsId} = await params
 if(parseInt(reviewsId)>1000){
-    // notFound()
+    notFound()
     redirect("/products")
 }
     return <h1> Review {reviewsId} for product {productId}</h1>
+}
 }
